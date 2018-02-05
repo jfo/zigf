c285e91c3163762dba8709726318a81815130f58
Jeff Fowler <jeffowler@gmail.com>
2018-02-06 00:00:14 +0100

    minus

--- main.zig
+++ main.zig
@@ -1,13 +1,12 @@
-const warn = @import("std").debug.warn;
-// main.zig
 pub fn main() void {
-  var mem = []u8{0} ** 30000;
-  const src = "+++++";
+    var mem = []u8{0} ** 30000;
+    const src = "+++++";
 
-  for (src) |c| {
-      switch(c) {
-          '+' => mem[0] += 1,
-          else => undefined
-      }
-  }
+    for (src) |c| {
+        switch(c) {
+            '+' => mem[0] += 1,
+            '-' => mem[0] -= 1,
+            else => undefined
+        }
+    }
 }
