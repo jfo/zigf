0f7fa50e103b1db28811dede77757c261fb6024a
Jeff Fowler <jeffowler@gmail.com>
2018-02-05 23:50:59 +0100

    make mem var

--- main.zig
+++ main.zig
@@ -1,7 +1,7 @@
 const warn = @import("std").debug.warn;
 // main.zig
 pub fn main() void {
-  const mem = []u8{0} ** 30000;
+  var mem = []u8{0} ** 30000;
   const src = "+++++";
 
   for (src) |c| {
