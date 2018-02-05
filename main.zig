e8e1a8e7f717c4740fb19b4688d3442cf0e7397f
Jeff Fowler <jeffowler@gmail.com>
2018-02-05 23:49:40 +0100

    warn in for loop

--- main.zig
+++ main.zig
@@ -1,5 +1,10 @@
+const warn = @import("std").debug.warn;
 // main.zig
 pub fn main() void {
   const mem = []u8{0} ** 30000;
   const src = "+++++";
+
+  for (src) |c| {
+      warn("{}", c);
+  }
 }
