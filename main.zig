2390de0e34f2ef22d53e1da20b5ba73d27643dee
Jeff Fowler <jeffowler@gmail.com>
2018-02-06 00:48:10 +0100

    split

--- main.zig
+++ main.zig
@@ -1,11 +1,5 @@
-const warn = @import("std").debug.warn;
-
-pub fn main() void {
-    var mem = []u8{0} ** 30000;
-    const src = "+++++";
-
+fn bf(src: []const u8, mem: [30000]u8) void {
     var memptr: u16 = 0;
-
     for (src) |c| {
         switch(c) {
             '+' => mem[memptr] += 1,
@@ -15,4 +9,10 @@ pub fn main() void {
             else => undefined
         }
     }
+}
+
+pub fn main() void {
+    var mem = []u8{0} ** 30000;
+    const src = "+++++";
+    bf(src, mem);
 }
