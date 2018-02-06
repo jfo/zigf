8ae906f0809dfbcc3e457487bdc827042b15fa7f
Jeff Fowler <jeffowler@gmail.com>
2018-02-06 22:47:41 +0100

    add executes test

--- main.zig
+++ main.zig
@@ -95,3 +95,11 @@ test "[] skips execution with internal braces and exits" {
     assert(storage[1] == 0);
 }
 
+test "[] executes with internal braces and exits" {
+    var storage = []u8{0} ** 2;
+    const src = "++[>++[-]++<-]";
+    bf(src, storage[0..]);
+    assert(storage[0] == 0);
+    assert(storage[1] == 2);
+}
+
