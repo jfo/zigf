998c744322bee1d16fa6a4c6ed278613f0ff7a91
Jeff Fowler <jeffowler@gmail.com>
2018-02-06 11:56:16 +0100

    add first test

--- main.zig
+++ main.zig
@@ -1,3 +1,5 @@
+const assert = @import("std").debug.assert;
+
 fn bf(src: []const u8, mem: []u8) void {
     var memptr: u16 = 0;
     for (src) |c| {
@@ -15,4 +17,11 @@ pub fn main() void {
     var mem = []u8{0} ** 30000;
     const src = "+++++";
     bf(src, mem[0..]);
+}
+
+test "+" {
+    var mem = []u8{0};
+    const src = "+++";
+    bf(src, mem[0..]);
+    assert(mem[0] == 3);
 }
