7a85e46c77075fa0cd907c5727e33c306f6d86ca
Jeff Fowler <jeffowler@gmail.com>
2018-02-10 18:31:24 +0100

    adding errors

--- bf.zig
+++ bf.zig
@@ -1,10 +1,11 @@
 const warn = @import("std").debug.warn;
+const sub = @import("std").math.sub;
 
-fn seekBack(src: []const u8, srcptr: u16) u16 {
+fn seekBack(src: []const u8, srcptr: u16) !u16 {
     var depth:u16 = 1;
     var ptr: u16 = srcptr - 1;
     while (depth > 0) {
-        ptr -= 1;
+        ptr = try sub(u16, ptr, 1);
         switch(src[ptr]) {
             '[' => depth -= 1,
             ']' => depth += 1,
@@ -28,7 +29,7 @@ fn seekForward(src: []const u8, srcptr: u16) u16 {
     return ptr;
 }
 
-pub fn bf(src: []const u8, storage: []u8) void {
+pub fn bf(src: []const u8, storage: []u8) !void {
     var memptr: u16 = 0;
     var srcptr: u16 = 0;
     while (srcptr < src.len) {
@@ -38,7 +39,7 @@ pub fn bf(src: []const u8, storage: []u8) void {
             '>' => memptr += 1,
             '<' => memptr -= 1,
             '[' => if (storage[memptr] == 0) srcptr = seekForward(src, srcptr),
-            ']' => if (storage[memptr] != 0) srcptr = seekBack(src, srcptr),
+            ']' => if (storage[memptr] != 0) srcptr = try seekBack(src, srcptr),
             '.' => warn("{c}", storage[memptr]),
             else => undefined
         }
