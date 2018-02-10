49b50d688b7f8459d330fe9dc1de047a3954a288
Jeff Fowler <jeffowler@gmail.com>
2018-02-10 10:20:21 +0100

    pull out function

--- bf.zig
+++ bf.zig
@@ -1,5 +1,22 @@
 const warn = @import("std").debug.warn;
 
+fn seekBack() {
+};
+
+fn seekForward(src: []const u8, srcptr: u16) u16 {
+    var depth:u16 = 1;
+    var ptr: u16 = srcptr + 1;
+    while (depth > 0) {
+        ptr += 1;
+        switch(src[ptr]) {
+            '[' => depth += 1,
+            ']' => depth -= 1,
+            else => undefined
+        }
+    }
+    return ptr;
+}
+
 pub fn bf(src: []const u8, storage: []u8) void {
     var memptr: u16 = 0;
     var srcptr: u16 = 0;
@@ -10,18 +27,7 @@ pub fn bf(src: []const u8, storage: []u8) void {
             '>' => memptr += 1,
             '<' => memptr -= 1,
             '.' => warn("{c}", storage[memptr]),
-            '[' => if (storage[memptr] == 0) {
-                var depth:u16 = 1;
-                srcptr += 1;
-                while (depth > 0) {
-                    srcptr += 1;
-                    switch(src[srcptr]) {
-                        '[' => depth += 1,
-                        ']' => depth -= 1,
-                        else => undefined
-                    }
-                }
-            },
+            '[' => if (storage[memptr] == 0) srcptr = seekForward(src, srcptr),
             ']' => if (storage[memptr] != 0) {
                 var depth:u16 = 1;
                 srcptr -= 1;
